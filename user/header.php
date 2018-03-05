327bdc2495e6f291e1cbfe126b1589e80fb32722
athuljosem <athuljosemenachery@gmail.com>
2018-03-05 15:49:41 +0530

    Header Edit

--- user/header.php
+++ user/header.php
@@ -127,7 +127,7 @@ require_once("../dbcon.php");?>
             <!-- /sidebar menu -->
 
             <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
+            <!-- <div class="sidebar-footer hidden-small">
               <a data-toggle="tooltip" data-placement="top" title="Settings">
                 <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
               </a>
@@ -140,7 +140,7 @@ require_once("../dbcon.php");?>
               <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
                 <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
               </a>
-            </div>
+            </div> -->
             <!-- /menu footer buttons -->
           </div>
         </div>
