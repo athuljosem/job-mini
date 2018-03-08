740339e8f73884d0e5d39b1ef1c2ce5d80dc1ce6
arjunkeshav <35555485+arjunkeshav@users.noreply.github.com>
2018-03-09 00:48:55 +0530

    user dashboard changed

--- user/dashboard.php
+++ user/dashboard.php
@@ -5,46 +5,12 @@
                 <div class="x_panel">
                   <div class="x_title">
                     <h2>QUALIFIED JOB POST</h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
+                    
                     <div class="clearfix"></div>
                   </div>
-                  <div class="x_content">
-                    <p class="text-muted font-13 m-b-30">
-                      The jobs posted by different companies but only that are matching to your qualifications gets listed here. Select and apply the jobs in which you are interested to attend.
-                  </p>
-          
-                    <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
-                      <thead>
-                        <tr>
-
-                          <th>Job Title</th>
-
-                          <th>Status</th>
-                          <!-- <th>Position</th> 
-                          <th>Office</th>
-                          <th>Age</th>
-                          <th>Start date</th>
-                          <th>Salary</th>
-                          <th>Extn.</th>
-                          <th>E-mail</th>-->
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <?php
+                  
+                
+               <?php
 
                         //FIX:There is a more easier way to do this Should fix it!!
 
@@ -55,7 +21,7 @@
                       if($result->num_rows > 0) {
                         while($row = $result->fetch_assoc()) 
                         {
-                        	$qualification=$row["qualification"];
+                          $qualification=$row["qualification"];
                         }
                     }
 $_SESSION['leftpanel'] = 'dashboard';
@@ -70,35 +36,72 @@ $_SESSION['leftpanel'] = 'dashboard';
                           $sql1 = "SELECT * FROM apply_job WHERE id_jobpost='$row[id_jobpost]' AND id_user='$_SESSION[userid]' ";
                            $result1 = $conn->query($sql1);
                       ?>
-                      <tr>
-                        <td><?php echo $row['jobtitle']; ?></td>
-                        <!-- <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><i class="fa fa-arrow-circle-right"></i></a></td> -->
-                        <?php  if($result1->num_rows > 0){ ?>
-                          <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><button class="btn btn_success bg-green" >Applied </button></td>
+
+               <?php 
+               $sql3 ="SELECT * FROM company WHERE id_company='$row[id_company]'";
+               $result3=$conn->query($sql3);
+
+                 if($result->num_rows > 0) {
+
+               $row3=$result3->fetch_assoc();
+               ?>
+
+<div class="col-md-4 col-sm-4 col-xs-12 profile_details">
+                        <div class="well profile_view">
+                          <div class="col-sm-12">
+                            <h1 class="brief"><i><?php echo $row3['companyname']; ?></i></h1>
+                            <div class="left col-xs-7">
+                              <h3>post:<?php echo $row['jobtitle']; ?></h3>
+                              
+                              <ul class="list-unstyled">
+                                <h2><li><i class="fa fa-inr"></i><?php echo $row['minimumsalary']; ?>-<?php echo $row['maximumsalary']; ?>  </li></h2>
+                                
+                              </ul>
+                            </div>
+                            <div class="right col-xs-5 text-center">
+                              <img src="../images/img.jpg" alt="" class="img-circle img-responsive">
+                            </div>
+                          </div>
+                          <div class="col-xs-12 bottom text-center">
+                            <div class="col-xs-12 col-sm-6 emphasis">
+                              <p class="ratings">
+                                <a>4.0</a>
+                                <a href="#"><span class="fa fa-star"></span></a>
+                                <a href="#"><span class="fa fa-star"></span></a>
+                                <a href="#"><span class="fa fa-star"></span></a>
+                                <a href="#"><span class="fa fa-star"></span></a>
+                                <a href="#"><span class="fa fa-star-o"></span></a>
+                              </p>
+                            </div>
+                            <div class="col-xs-12 col-sm-6 emphasis">
+                              
+                              <?php  if($result1->num_rows > 0){ ?>
+                          <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><button class="btn btn_success bg-green" >Applied </button></a></td>
                     
                           <?php }
                           else
                           {
                           ?>
-                          <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><button class="btn btn_success bg-red" >Not Applied </button></td>
+                          <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><button class="btn btn_success bg-red" >Not Applied </button></a></td>
                           <?php }
                         
                           ?>
-                      </tr>
-                      <?php
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                  
+ <?php
+                       }
                        }
                      }
                      ?>
-                        </tbody>
-                    </table>
-          
-          
-                  </div>
-                </div>
-              </div>
+                   </div>
+               </div>
             </div>
-          </div>
-        </div>
+        
+      
 
          <!-- /page content -->
           
