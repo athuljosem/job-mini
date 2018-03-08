2f4101ef5a03931162f14dd28780e2dd3ad2785f
athuljosem <athuljosemenachery@gmail.com>
2018-03-08 11:39:41 +0530

    Mail Box Added

---
+++ company/mailbox.php
@@ -0,0 +1,250 @@
+<?php include 'header.php' ?>
+<div class="right_col" role="main">
+          <div class="">
+
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Your Mails </h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  
+                </div>
+              </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <div class="row">
+              <div class="col-md-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Notification Mails</h2>
+                    
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <div class="row">
+                      <div class="col-sm-3 mail_list_column">
+                        
+                        <a data-toggle="tab" href="#menu45">
+                          <div class="mail_list">
+                            <div class="left">
+                              <i class="fa fa-circle"></i> 
+                              <!-- <i class="fa fa-edit"></i> -->
+                            </div>
+                            <div class="right">
+                              <h3>Company Name <small>3.00 PM</small></h3>
+                              <p>The Content of the mail comes here</p>
+                            </div>
+                          </div>
+                        </a>
+                        <a data-toggle="tab" href="#menu2">
+                          <div class="mail_list">
+                            <div class="left">
+                              .
+                            </div>
+                            <div class="right">
+                              <h3>Company 2<small>4.09 PM</small></h3>
+                              <p>Mail Content 2</p>
+                            </div>
+                          </div>
+                        </a>
+
+                      </div>
+                      <!-- /MAIL LIST -->
+
+                      <!-- CONTENT MAIL -->
+                      <div class="col-sm-9 mail_view tab-content">
+                        <div id="menu45" class="inbox-body tab-pane fade in active">
+                          <div class="mail_heading row">
+                            <div class="col-md-8">
+                              <div class="btn-group">
+                                <button class="btn btn-sm btn-primary" type="button"><i class="fa fa-reply"></i> Reply</button>
+                              </div>
+                            </div>
+                            <div class="col-md-4 text-right">
+                              <p class="date"> 8:02 PM 12 FEB 2014</p>
+                            </div>
+                            <div class="col-md-12">
+                              <h4> Mail Title</h4>
+                            </div>
+                          </div>
+                          <div class="sender-info">
+                            <div class="row">
+                              <div class="col-md-12">
+                                <strong>Company Name</strong>
+                                <span>(companymail@gmail.com)</span> to
+                                <strong>me</strong>
+                                <!-- <a class="sender-dropdown"><i class="fa fa-chevron-down"></i></a> -->
+                                <br><br>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="view-mail">
+                            <p>The Mail Content Comes Here</p>
+                            
+                          </div>
+                          <div class="attachment">
+                            <p>
+                              <span><i class="fa fa-paperclip"></i> 3 attachments — </span>
+                              <a href="#">Download all attachments</a> |
+                              <a href="#">View all images</a>
+                            </p>
+                            <ul>
+                              <li>
+                                <a href="#" class="atch-thumb">
+                                  <img src="images/inbox.png" alt="img" />
+                                </a>
+
+                                <div class="file-name">
+                                  image-name.jpg
+                                </div>
+                                <span>12KB</span>
+
+
+                                <div class="links">
+                                  <a href="#">View</a> -
+                                  <a href="#">Download</a>
+                                </div>
+                              </li>
+
+                              <li>
+                                <a href="#" class="atch-thumb">
+                                  <img src="images/inbox.png" alt="img" />
+                                </a>
+
+                                <div class="file-name">
+                                  img_name.jpg
+                                </div>
+                                <span>40KB</span>
+
+                                <div class="links">
+                                  <a href="#">View</a> -
+                                  <a href="#">Download</a>
+                                </div>
+                              </li>
+                              <li>
+                                <a href="#" class="atch-thumb">
+                                  <img src="images/inbox.png" alt="img" />
+                                </a>
+
+                                <div class="file-name">
+                                  img_name.jpg
+                                </div>
+                                <span>30KB</span>
+
+                                <div class="links">
+                                  <a href="#">View</a> -
+                                  <a href="#">Download</a>
+                                </div>
+                              </li>
+
+                            </ul>
+                          </div>
+                          <div class="btn-group">
+                            <button class="btn btn-sm btn-primary" type="button"><i class="fa fa-reply"></i> Reply</button>
+                          </div>
+                        </div>
+                        <div id="menu2" class="inbox-body tab-pane fade">
+                          <div class="mail_heading row">
+                            <div class="col-md-8">
+                              <div class="btn-group">
+                                <button class="btn btn-sm btn-primary" type="button"><i class="fa fa-reply"></i> Reply</button>
+                              </div>
+                            </div>
+                            <div class="col-md-4 text-right">
+                              <p class="date"> 8:02 PM 12 FEB 2014</p>
+                            </div>
+                            <div class="col-md-12">
+                              <h4> Mail  2</h4>
+                            </div>
+                          </div>
+                          <div class="sender-info">
+                            <div class="row">
+                              <div class="col-md-12">
+                                <strong>Company2</strong>
+                                <span>(companymail2@gmail.com)</span> to
+                                <strong>me</strong>
+                                <!-- <a class="sender-dropdown"><i class="fa fa-chevron-down"></i></a> -->
+                                <br><br>
+                              </div>
+                            </div>
+                          </div>
+                          <div class="view-mail">
+                            <p>The Mail Content 2 Comes Here</p>
+                            
+                          </div>
+                          <div class="attachment">
+                            <p>
+                              <span><i class="fa fa-paperclip"></i> 3 attachments — </span>
+                              <a href="#">Download all attachments</a> |
+                              <a href="#">View all images</a>
+                            </p>
+                            <ul>
+                              <li>
+                                <a href="#" class="atch-thumb">
+                                  <img src="images/inbox.png" alt="img" />
+                                </a>
+
+                                <div class="file-name">
+                                  image-name.jpg
+                                </div>
+                                <span>12KB</span>
+
+
+                                <div class="links">
+                                  <a href="#">View</a> -
+                                  <a href="#">Download</a>
+                                </div>
+                              </li>
+
+                              <li>
+                                <a href="#" class="atch-thumb">
+                                  <img src="images/inbox.png" alt="img" />
+                                </a>
+
+                                <div class="file-name">
+                                  img_name.jpg
+                                </div>
+                                <span>40KB</span>
+
+                                <div class="links">
+                                  <a href="#">View</a> -
+                                  <a href="#">Download</a>
+                                </div>
+                              </li>
+                              <li>
+                                <a href="#" class="atch-thumb">
+                                  <img src="images/inbox.png" alt="img" />
+                                </a>
+
+                                <div class="file-name">
+                                  img_name.jpg
+                                </div>
+                                <span>30KB</span>
+
+                                <div class="links">
+                                  <a href="#">View</a> -
+                                  <a href="#">Download</a>
+                                </div>
+                              </li>
+
+                            </ul>
+                          </div>
+                          <div class="btn-group">
+                            <button class="btn btn-sm btn-primary" type="button"><i class="fa fa-reply"></i> Reply</button>
+                          </div>
+                        </div>
+
+                      </div>
+                      <!-- /CONTENT MAIL -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- <?php include 'footer.php' ?> -->
