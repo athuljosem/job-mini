261baeef373c7b7fc0775d4bb2fa15c12c2c5ae3
athuljosem <athuljosemenachery@gmail.com>
2018-03-19 19:01:52 +0530

    header

--- user/header.php
+++ user/header.php
@@ -172,9 +172,6 @@ if($result->num_rows > 0) {
                   </ul>
                 </li>
 
-
-
-
                 <li role="presentation" class="dropdown">
                   <a href="javascript:;" class="dropdown-toggle info-number" data-toggle="dropdown" aria-expanded="false">
                     <i class="fa fa-envelope-o"></i>
