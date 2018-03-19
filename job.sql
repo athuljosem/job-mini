040ec4c9fc34ed82ebd9878adc197f4b4305a3cb
athuljosem <athuljosemenachery@gmail.com>
2018-03-19 14:37:39 +0530

    Qualification Table added

--- job.sql
+++ job.sql
@@ -1,22 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
+-- version 3.3.9
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 18, 2018 at 11:01 AM
--- Server version: 5.7.20-log
--- PHP Version: 5.6.31
+-- Host: localhost
+-- Generation Time: Mar 19, 2018 at 09:06 AM
+-- Server version: 5.5.28
+-- PHP Version: 5.3.5
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `job`
@@ -28,13 +25,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-DROP TABLE IF EXISTS `admin`;
 CREATE TABLE IF NOT EXISTS `admin` (
   `id_admin` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   PRIMARY KEY (`id_admin`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `admin`
@@ -49,26 +45,20 @@ INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
 -- Table structure for table `apply_job`
 --
 
-DROP TABLE IF EXISTS `apply_job`;
 CREATE TABLE IF NOT EXISTS `apply_job` (
   `id_applyjob` int(11) NOT NULL AUTO_INCREMENT,
   `id_company` int(11) NOT NULL,
   `id_jobpost` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
   PRIMARY KEY (`id_applyjob`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Dumping data for table `apply_job`
 --
 
 INSERT INTO `apply_job` (`id_applyjob`, `id_company`, `id_jobpost`, `id_user`) VALUES
-(1, 1, 1, 1),
-(2, 1, 2, 1),
-(3, 1, 1, 2),
-(4, 1, 2, 2),
-(7, 1, 1, 1),
-(9, 1, 2, 1);
+(10, 1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -76,7 +66,6 @@ INSERT INTO `apply_job` (`id_applyjob`, `id_company`, `id_jobpost`, `id_user`) V
 -- Table structure for table `company`
 --
 
-DROP TABLE IF EXISTS `company`;
 CREATE TABLE IF NOT EXISTS `company` (
   `id_company` int(11) NOT NULL AUTO_INCREMENT,
   `companyname` varchar(255) NOT NULL,
@@ -94,14 +83,14 @@ CREATE TABLE IF NOT EXISTS `company` (
   `active` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id_company`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `company`
 --
 
 INSERT INTO `company` (`id_company`, `companyname`, `contactno`, `website`, `companytype`, `email`, `password`, `country`, `state`, `city`, `aboutme`, `logo`, `createdAt`, `active`) VALUES
-(1, 'google', '1234567890', 'www.google.com', NULL, 'google@gmail.com', '1234', 'india', 'kerala', 'ernakulam', 'well known company', NULL, '2018-02-22 17:43:59', 1);
+(1, 'google', '1234567890', 'www.google.com', NULL, 'google@gmail.com', '1234', 'india', 'kerala', 'ernakulam', 'well known company', NULL, '2018-02-22 23:13:59', 1);
 
 -- --------------------------------------------------------
 
@@ -109,7 +98,6 @@ INSERT INTO `company` (`id_company`, `companyname`, `contactno`, `website`, `com
 -- Table structure for table `company_mailbox`
 --
 
-DROP TABLE IF EXISTS `company_mailbox`;
 CREATE TABLE IF NOT EXISTS `company_mailbox` (
   `id_mail` int(11) NOT NULL AUTO_INCREMENT,
   `id_company` int(11) NOT NULL,
@@ -118,16 +106,16 @@ CREATE TABLE IF NOT EXISTS `company_mailbox` (
   `mail_content` varchar(1000) CHARACTER SET utf8 NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_mail`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `company_mailbox`
 --
 
 INSERT INTO `company_mailbox` (`id_mail`, `id_company`, `id_jobpost`, `mail_title`, `mail_content`, `createdAt`) VALUES
-(1, 1, 2, 'Test Title', 'Testing', '2018-03-17 09:34:26'),
-(2, 1, 2, 'AAA', '<p>abc</p>', '2018-03-17 10:09:11'),
-(3, 1, 1, 'test 2', '<p>test 2</p>', '2018-03-17 10:09:45');
+(1, 1, 2, 'Test Title', 'Testing', '2018-03-17 15:04:26'),
+(2, 1, 2, 'AAA', '<p>abc</p>', '2018-03-17 15:39:11'),
+(3, 1, 1, 'test 2', '<p>test 2</p>', '2018-03-17 15:39:45');
 
 -- --------------------------------------------------------
 
@@ -135,7 +123,6 @@ INSERT INTO `company_mailbox` (`id_mail`, `id_company`, `id_jobpost`, `mail_titl
 -- Table structure for table `job_post`
 --
 
-DROP TABLE IF EXISTS `job_post`;
 CREATE TABLE IF NOT EXISTS `job_post` (
   `id_jobpost` int(11) NOT NULL AUTO_INCREMENT,
   `id_company` int(11) NOT NULL,
@@ -147,15 +134,15 @@ CREATE TABLE IF NOT EXISTS `job_post` (
   `qualification` varchar(255) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_jobpost`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Dumping data for table `job_post`
 --
 
 INSERT INTO `job_post` (`id_jobpost`, `id_company`, `jobtitle`, `description`, `minimumsalary`, `maximumsalary`, `experience`, `qualification`, `createdAt`) VALUES
-(1, 1, 'bpo', '<p>well-knowlede in java,php and dbms</p>', '20000', '50000', '0', 'mca', '2018-02-24 11:24:44'),
-(2, 1, 'manager', '<p>The managing post of ernakulam branch</p>', '40000', '50000', '5', 'btech', '2018-03-01 06:10:21');
+(1, 1, 'bpo', '<p>well-knowlede in java,php and dbms</p>', '20000', '50000', '0', 'mca', '2018-02-24 16:54:44'),
+(2, 1, 'manager', '<p>The managing post of ernakulam branch</p>', '40000', '50000', '5', 'btech', '2018-03-01 11:40:21');
 
 -- --------------------------------------------------------
 
@@ -163,7 +150,6 @@ INSERT INTO `job_post` (`id_jobpost`, `id_company`, `jobtitle`, `description`, `
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `fname` varchar(255) NOT NULL,
@@ -185,7 +171,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `active` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `users`
@@ -202,7 +188,6 @@ INSERT INTO `users` (`user_id`, `fname`, `lname`, `email`, `password`, `address`
 -- Table structure for table `user_mailbox`
 --
 
-DROP TABLE IF EXISTS `user_mailbox`;
 CREATE TABLE IF NOT EXISTS `user_mailbox` (
   `id_usermail` int(11) NOT NULL,
   `id_user` int(11) NOT NULL,
@@ -212,8 +197,34 @@ CREATE TABLE IF NOT EXISTS `user_mailbox` (
   `mail_content` text NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Dumping data for table `user_mailbox`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_qualification`
+--
+
+CREATE TABLE IF NOT EXISTS `user_qualification` (
+  `q_id` int(5) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `q_level` varchar(200) NOT NULL,
+  `qualification` varchar(200) NOT NULL,
+  `subject` varchar(200) NOT NULL,
+  `institution` varchar(200) NOT NULL,
+  `university` varchar(200) NOT NULL,
+  `percentage` float NOT NULL,
+  `grade` varchar(2) NOT NULL,
+  `passout` date NOT NULL,
+  `register_number` int(100) NOT NULL,
+  PRIMARY KEY (`q_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `user_qualification`
+--
+
