aa406e31c2357ab61847e19c0e086c507053e199
Ivin Stephen <ivinstephen001@gmail.com>
2018-03-21 05:04:51 +0530

    edit qualifcations

--- user/qualification.php
+++ user/qualification.php
@@ -7,6 +7,10 @@
     <div class="x_panel">
       <div class="x_title">
         <h2>Qualification</h2>
+        <!-- Trigger the modal with a button -->
+        <div class="" style="float: right;">
+          <button type="button" class="btn btn-info" data-toggle="modal" data-target="#myModal">+</button>
+        </div>
         <div class="clearfix"></div>        
       </div>
 
@@ -33,8 +37,6 @@
                   <b><?php echo $row['institution'];?></b>(<?php echo $row['university'];?>) <br>
                   <i>Passout-<?php echo $row['passout'];?></i> 
               </div> 
-              <!-- <div class="panel-footer"> -->
-              <!-- </div> -->
             </div>
           <?php endwhile ?>
         <?php endif ?>
@@ -43,57 +45,74 @@
       
       
       <!-- form to enter the qualifications -->
-      <div class="col-md-7">
-        <div class="well">
-          <form id="qual_form" method="post" action="insertqualification.php">
-            <div class="form-group">
-              <label for="q_level" >Qualification Level</label>
-              <input type="text" class="form-control" id="q_level" name="q_level" >
-            </div>
-            <div class="form-group">
-              <label for="qualification_id" >qualification</label>
-              <input type="text" class="form-control" id="qualification_id" name="qualification" >
-            </div>
+      
 
-            <div class="form-group">
-              <label for="subject_id" >subject</label>
-              <input type="text" class="form-control" id="subject_id" name="subject" >
-            </div>
 
-            <div class="form-group">
-              <label for="institution_id" >institution</label>
-              <input type="text" class="form-control" id="institution_id" name="institution" >
-            </div>
+      <!-- Modal -->
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
 
-            <div class="form-group">
-              <label for="university_id" >university</label>
-              <input type="text" class="form-control" id="university_id" name="university" >
-            </div>
+          <!-- Modal content-->
+          <div class="modal-content">
+            <form id="qual_form" method="post" action="insertqualification.php">
+                
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Modal Header</h4>
+                </div>
 
-            <div class="form-group">
-              <label for="percentage_id" >percentage</label>
-              <input type="text" class="form-control" id="percentage_id" name="percentage" >
-            </div>
+                <div class="modal-body">
 
-            <div class="form-group">
-              <label for="grade_id" >grade</label>
-              <input type="text" class="form-control" id="grade_id" name="grade" >
-            </div>
+                  <div class="form-group">
+                    <label for="q_level" >Qualification Level</label>
+                    <input type="text" class="form-control" id="q_level" name="q_level" >
+                  </div>
+                  <div class="form-group">
+                    <label for="qualification_id" >qualification</label>
+                    <input type="text" class="form-control" id="qualification_id" name="qualification" >
+                  </div>
+                  <div class="form-group">
+                    <label for="subject_id" >subject</label>
+                    <input type="text" class="form-control" id="subject_id" name="subject" >
+                  </div>
+                  <div class="form-group">
+                    <label for="institution_id" >institution</label>
+                    <input type="text" class="form-control" id="institution_id" name="institution" >
+                  </div>
+                  <div class="form-group">
+                    <label for="university_id" >university</label>
+                    <input type="text" class="form-control" id="university_id" name="university" >
+                  </div>
+                  <div class="form-group">
+                    <label for="percentage_id" >percentage</label>
+                    <input type="text" class="form-control" id="percentage_id" name="percentage" >
+                  </div>
+                  <div class="form-group">
+                    <label for="grade_id" >grade</label>
+                    <input type="text" class="form-control" id="grade_id" name="grade" >
+                  </div>
+                  <div class="form-group">
+                    <label for="passout_id" >passout</label>
+                    <input type="text" class="form-control" id="passout_id" name="passout" >
+                  </div>
+                  <div class="form-group">
+                    <label for="register_number_id" >register_number</label>
+                    <input type="text" class="form-control" id="register_number_id" name="register_number" >
+                  </div>
 
-            <div class="form-group">
-              <label for="passout_id" >passout</label>
-              <input type="text" class="form-control" id="passout_id" name="passout" >
-            </div>
+                </div>
 
-            <div class="form-group">
-              <label for="register_number_id" >register_number</label>
-              <input type="text" class="form-control" id="register_number_id" name="register_number" >
-            </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success" name="submit">submit</button>
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
 
-            <button  class="btn btn-success" name="submit">submit</button>
-          </form>
+            </form>
+          </div>
+
         </div>
       </div>
+
     </div>
   </div>
 
@@ -122,3 +141,26 @@
   });
 </script>
 
+<!-- Trigger the modal with a button -->
+<button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
