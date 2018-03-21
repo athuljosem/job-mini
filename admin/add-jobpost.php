659a8857414082518877c568a401c80fd4af664e
arjunkeshav <35555485+arjunkeshav@users.noreply.github.com>
2018-03-21 09:33:11 +0530

    add job post

---
+++ admin/add-jobpost.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+if(empty($_SESSION['id_admin'])) {
+	header("Location: index.php");
+	exit();
+}
+
+
+require_once("../dbcon.php");
+
+if(isset($_GET)) {
+
+	//Delete Company using id and redirect
+	$sql = "UPDATE job_post SET active='1' WHERE id_company='$_GET[id]'";
+	if($conn->query($sql)) {
+		header("Location: dashboard.php");
+		exit();
+	} else {
+		echo "Error";
+	}
+}
