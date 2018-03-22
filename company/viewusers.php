0f31ae7939d2f9c973e7a470f8e1658813a9ea4a
athuljosem <athuljosemenachery@gmail.com>
2018-03-22 23:54:39 +0530

    View Users Fixed

--- company/viewusers.php
+++ company/viewusers.php
@@ -25,44 +25,109 @@
               <th>Contact</th>
               <th>Highest Qualification</th>
               <th>Passing Year</th>
-              <th>More</th>
+              <!-- <th>More</th> -->
             </tr>
           </thead>
           <tbody>
             <?php
-                        $sql = "SELECT * FROM apply_job WHERE id_jobpost='$_GET[id]'";
-                        $result = $conn->query($sql);
+            $sql = "SELECT * FROM apply_job WHERE id_jobpost='$_GET[id]'";
+            $result = $conn->query($sql);
 
-                        if($result->num_rows > 0) {
-                         while($row = $result->fetch_assoc()) 
-                         {
-                          $sql1 = "SELECT * FROM users WHERE user_id='$row[id_user]'";
-                          $result1 = $conn->query($sql1);
-                          $row1 = $result1->fetch_assoc();
-                          
-                          
-                          ?>
-                          <tr>
-                           
-                            <td><?php echo $row1['fname'] ; ?> <?php echo $row1['lname']; ?></td>
-                            <td><?php echo $row1['email'] ; ?> </td>
-                            <td><?php echo $row1['contactno'] ; ?></td>
-                            <td><?php echo $row1['qualification'] ; ?> </td>
-                            <td><?php echo $row1['passingyear'] ; ?> </td>
-                            <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><i class="fa fa-arrow-circle-right"></i></a></td>
-                          </tr>
-                          <?php
-                          
-                        }
-                      }
-                      ?>
+            if($result->num_rows > 0) {
+             while($row = $result->fetch_assoc()) 
+             {
+              $sql1 = "SELECT * FROM users WHERE user_id='$row[id_user]'";
+              $result1 = $conn->query($sql1);
+              $row1 = $result1->fetch_assoc();
+
+
+              ?>
+              <tr>
+
+                <td>
+                  <span class="fa fa-plus-circle text-primary fa-lg" data-toggle="modal" data-target="#<?php echo $row1['user_id'] ; ?>"></span>
+
+
+                  <?php echo $row1['fname'] ; ?> <?php echo $row1['lname']; ?></td>
+                  <td><?php echo $row1['email'] ; ?> </td>
+                  <td><?php echo $row1['contactno'] ; ?></td>
+                  <td><?php echo $row1['qualification'] ; ?> </td>
+                  <td><?php echo $row1['passingyear'] ; ?> </td>
+                  <!-- <td><a href="view.php?id=<?php echo $row['id_jobpost']; ?>"><i class="fa fa-arrow-circle-right"></i></a></td> -->
+                </tr>
+                <?php
+
+              }
+            }
+            ?>
             
 
           </tbody>
         </table>
-      </div>
+
+        <?php
+        $sql2 = "SELECT * FROM apply_job WHERE id_jobpost='$_GET[id]'";
+        $result2 = $conn->query($sql2);
+
+        if($result2->num_rows > 0) 
+        {
+         while($row2 = $result2->fetch_assoc()) 
+         {
+
+          ?>
+          <div id="<?php echo $row2['id_user'];?>" class="modal fade" role="dialog">
+            <div class="modal-dialog">
+
+              <!-- Modal content-->
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Qualifications</h4>
+                </div>
+
+                <div class="modal-body">
+                  <!-- athul put your qualifictions here -->
+                  <ui class="list-group">
+                    <?php
+                    $sql3 = "SELECT * FROM user_qualification WHERE user_id='$row2[id_user]'";
+                    $result3 = $conn->query($sql3);
+                    if($result3->num_rows > 0) 
+                    {
+                      while($row3 = $result3->fetch_assoc())
+                      {
+                        ?>
+
+                        <li class="list-group-item"><b><?php echo $row3['q_level']."-".$row3['qualification']."(".$row3['subject']."):";?>
+                      </b><?php echo $row3['institution']."(".$row3['university'].")-";?><b><?php echo $row3['percentage']; ?>% </b>
+<?php echo $row3['passout'];?> Passout
+                    </li>
+                       <!--  <li class="list-group-item">M.tech</li>
+                        <li class="list-group-item">N.tech</li>
+                        <li class="list-group-item">L.tech</li>
+                        <li class="list-group-item">O.tech</li> -->
+                        <?php
+                      }
+                    }
+                    ?>
+                  </ui>
+                </div>
+
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+              </div>
+
+
+            </div>
+          </div>
+
+          <?php
+        }
+      }
+      ?>
     </div>
   </div>
+</div>
 </div>
 
 
